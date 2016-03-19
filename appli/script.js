2abdf71eaea0cba7cffe2f5a95eb3759a3bf0722
KellGlycol <vahic69@gmail.com>
2016-03-19 02:11:37 +0100

    Ajout du sourcing externe + page d'accueil

--- appli/script.js
+++ appli/script.js
@@ -4,22 +4,21 @@ function main() {
 
 
 	var feedTest = new RssFeed();
-	var articleTest = new RssArticle("Le premier article","aled.jpg","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non tellus dignissim, pharetra mauris ac, ultricies tellus. Praesent bibendum tempus nibh. Nullam id tortor quis elit pellentesque pharetra. Sed at urna sit amet diam congue sodales. Aenean vel leo nec tellus rhoncus pulvinar posuere et lectus. Nam consectetur, arcu efficitur tincidunt varius, felis risus vehicula orci, non tempor sem erat ac lorem. Aenean eu convallis dui. Suspendisse sit amet dapibus neque, ac egestas lorem. Sed vehicula turpis libero, vitae ullamcorper diam interdum sed. Sed et lacus at libero condimentum maximus non eget enim. Nunc ac luctus nunc. Aliquam ultricies suscipit iaculis. Aenean ultricies ex quis massa lacinia, a maximus nunc egestas. Phasellus ac nisi id libero blandit commodo. Maecenas ultrices diam mi.Morbi id fermentum magna. Vivamus gravida ante ac mi vehicula, vel auctor augue tempor. Maecenas eu blandit elit. Mauris tempus, urna in dignissim blandit, elit leo rutrum tellus, quis suscipit ligula ante sed mi. Praesent nec sem in velit tincidunt lacinia. Praesent ornare tristique lacus a finibus. Quisque scelerisque pellentesque nunc, id tincidunt nulla eleifend ac. Cras a ex laoreet, iaculis massa vel, tincidunt risus. Sed sit amet sagittis felis. Pellentesque at tortor metus. Maecenas id semper mi, in ultrices ipsum. Phasellus cursus ante porttitor nulla maximus, ut lacinia dui scelerisque. Aenean velit nisl, ornare ut lectus nec, eleifend congue felis. Proin vitae turpis vitae purus convallis tincidunt.Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce lacus sem, pharetra sed placerat eget, vestibulum sed mauris. Cras varius tristique ligula, a tempor lacus. Curabitur semper, erat consequat fringilla lobortis, mi nisl vulputate orci, eget auctor sem elit vel magna. Quisque sed imperdiet tortor. Donec ante dolor, facilisis et nulla a, vulputate maximus metus. Vivamus pellentesque dui sit amet mauris maximus porttitor. Suspendisse varius laoreet luctus. Proin id metus aliquet, ullamcorper odio sed, euismod felis. Maecenas sodales vulputate velit, dapibus cursus leo pharetra non.Aliquam elementum sodales euismod. Nulla vel fermentum lectus. Nam at quam nec libero pharetra interdum. Nam suscipit urna arcu, non efficitur sapien rutrum ac. Duis commodo dictum risus, ut tempor eros maximus non. Vestibulum faucibus nibh auctor tincidunt bibendum. Integer eleifend vulputate massa, ut rutrum mauris suscipit ac. Fusce ut dui et leo aliquet finibus. Aenean nec congue neque.","18/03/2016","<video controls><source src='https://pbs.twimg.com/tweet_video/CdzBkN4UkAAxcLf.mp4' type='video/mp4' /></video>");
-	feedTest.addArticle(articleTest);
 	
+	document.getElementById("feedAdressPrompter").style.display = "none";
 	
-	feedTest.extractRssFrom("assets/RSSTest.xml");
-	feedTest.display();
+	feedTest.extractRssFrom("https://crossorigin.me/http://radiofrance-podcast.net/podcast09/rss_11591.xml");
+	
 }
 
 
-function RssArticle(title,linkTag,description,pubDate,media)
+function RssArticle()
 {
-	this.title=title;
-	this.linkTag=linkTag;
-	this.description=description;
-	this.pubDate=pubDate;
-	this.media=media;
+	this.title="";
+	this.linkTag="";
+	this.description="";
+	this.pubDate="";
+	this.media="";
 
 	
 	this.buildHtmlDisplay = function()
@@ -29,10 +28,10 @@ function RssArticle(title,linkTag,description,pubDate,media)
 		if(this.title!=="")
 		{
 			htmlCode+="<h2>"
-			if(linkTag!==""){htmlCode+="<a href='"+this.linkTag+"'>";}
+			if(this.linkTag!==""){htmlCode+="<a href='"+this.linkTag+"'>";}
 			htmlCode+=this.title;
-			if(linkTag!==""){htmlCode+="</a>";}
-			if(this.pubDate!==""){htmlCode+="<span class='articleDate'> - "+this.pubDate+"</span>";}
+			if(this.linkTag!==""){htmlCode+="</a>";}
+			if(this.pubDate!==""){htmlCode+="<span class='articleDate text-muted'> - "+this.pubDate+"</span>";}
 			htmlCode+="</h2>";
 		}
 		
@@ -52,6 +51,25 @@ function RssArticle(title,linkTag,description,pubDate,media)
 		return htmlCode;
 	}
 	
+	this.articleUpdateFromXML = function(xmlInput)
+	{
+		if(xmlInput.getElementsByTagName("title")[0]!=null){this.title=xmlInput.getElementsByTagName("title")[0].textContent;}
+		if(xmlInput.getElementsByTagName("link")[0]!=null){this.linkTag=xmlInput.getElementsByTagName("link")[0].textContent;}
+		if(xmlInput.getElementsByTagName("description")[0]!=null){this.description=xmlInput.getElementsByTagName("description")[0].textContent;}
+		if(xmlInput.getElementsByTagName("pubDate")[0]!=null){this.pubDate=xmlInput.getElementsByTagName("pubDate")[0].textContent;}
+		
+		if(xmlInput.getElementsByTagName("enclosure")[0]!=null)
+		{
+			this.media=	"<audio controls preload='none' src='"+
+						xmlInput.getElementsByTagName("enclosure")[0].getAttribute("url")+
+						"' type='"+
+						xmlInput.getElementsByTagName("enclosure")[0].getAttribute("type")+
+						"'>Your browser do not support HTML5 audio tag :(</audio>";
+			
+		
+		}
+		
+	}
 }
 
 
@@ -70,7 +88,7 @@ function RssFeed()
 	
 	this.display = function()
 	{
-		document.getElementById("rssHeader").innerHTML = "<div class='row'><div class='col-md-2'></div><div class='col-md-8'><h2><a href='"+this.linkTag+"'>"+this.title+"</a></h2></div></div>";
+		document.getElementById("rssHeader").innerHTML = "<div class='row'><div class='col-md-2'></div><div class='col-md-8'><h1><a href='"+this.linkTag+"'>"+this.title+"</a></h1></div></div>";
 		document.getElementById("rssHeader").innerHTML += "<div class='row'><div class='col-md-2'></div><div class='col-md-8'><p id='feedDescription'>"+this.description+"</p></div></div>";
 		
 		document.getElementById("rssFeed").innerHTML = "";
@@ -81,25 +99,22 @@ function RssFeed()
 		
 	}
 	
-	this.fieldsUpdateFromXML = function(xmlInput)
-	{
-		this.title=rssContent.getElementsByTagName("title")[0].textContent;
-		this.linkTag=rssContent.getElementsByTagName("link")[0].textContent;
-		this.description=rssContent.getElementsByTagName("description")[0].textContent;
-	}
-	
 	this.extractRssFrom = function(fileUrl)
 	{
 		var req = new XMLHttpRequest();
 		req.open("GET",fileUrl);
 		
+		
+		
+		var objectReference = this;
+		
 		req.onreadystatechange = function() {
-			if(req.readyState === 4)
+			 if(req.readyState === 4)
 			{
 				if(req.status === 200)
 				{
-					console.log(this);
-					fieldsUpdateFromXML(req.responseXML);
+					
+					objectReference.feedUpdateFromXML(req.responseXML);
 				}
 				else
 				{
@@ -112,6 +127,33 @@ function RssFeed()
 		req.send();
 	}
 	
+	this.feedUpdateFromXML = function(xmlInput)
+	{
+		if(xmlInput!=null)
+		{
+			this.title=xmlInput.getElementsByTagName("title")[0].textContent;
+			this.linkTag=xmlInput.getElementsByTagName("link")[0].textContent;
+			this.description=xmlInput.getElementsByTagName("description")[0].textContent;
+		
+			articlesList = xmlInput.getElementsByTagName("item");
+			
+			var i=0;
+			
+			for(var elem of articlesList)
+			{
+				var article = new RssArticle()
+				article.articleUpdateFromXML(elem);
+				this.addArticle(article);
+				i++;
+				if(i>20)
+				{
+					break;
+				}
+			}
+			
+			this.display();
+		}
+	}
 	
 	
 }
